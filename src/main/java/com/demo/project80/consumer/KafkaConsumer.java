e80a88e0fd234b4691dcaf31506e37a83b2efa0a
Arjun Surendra <asurendra@vmware.com>
2022-04-19 17:11:31 +0530

    code

---
+++ src/main/java/com/demo/project80/consumer/KafkaConsumer.java
@@ -0,0 +1,34 @@
+package com.demo.project80.consumer;
+
+import java.util.concurrent.TimeUnit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+public class KafkaConsumer implements ApplicationRunner {
+
+    private static final String TOPIC_NAME = "mytopic";
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumer.class, args);
+    }
+
+    @KafkaListener(topics = TOPIC_NAME, groupId = "test-group")
+    public void topicConsumer(ConsumerRecord<?, ?> userRecord) {
+        log.info("Received : {} ", userRecord);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        while (true) {
+            TimeUnit.SECONDS.sleep(60);
+        }
+    }
+}
